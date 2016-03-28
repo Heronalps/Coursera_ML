b4fc4d3c622213eeb3be80cb692de313c13ce2c1
Michael Zhang <heronalps@gmail.com>
2016-03-28 07:53:00 -0400

    Commit_20160328

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,14 +20,17 @@ grad = zeros(size(theta));
 %
 
 
+hypo = X * theta;
+
+J = (1/(2*m)) * (sum((hypo-y).^2)) + (lambda/(2*m)) * (sum(theta(2:end,:).^2));
+
+% Summation's priority is lower than multiplication operator
+% Matrix multiplication X'*(hypo-y) does the summation for vector (So delicate!!!)
 
 
+% Set theta(1) to 0 without changing the original theta
 
-
-
-
-
-
+grad = (1/m) * (X' * (hypo-y)) + (lambda/m) * [0; theta(2:end,:)];
 
 
 % =========================================================================
