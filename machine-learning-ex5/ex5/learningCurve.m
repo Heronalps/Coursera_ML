2a73629b6986ba3de35558101d733565b89ab34f
Michael Zhang <heronalps@gmail.com>
2016-03-29 08:02:24 -0400

    Commit_20160329

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,6 +53,18 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+theta = zeros(size(X,2));
+
+
+for i = 1:m
+    
+    theta = trainLinearReg(X(1:i,:),y(1:i),lambda);
+    
+    error_train(i,1) = linearRegCostFunction(X(1:i,:),y(1:i),theta,0);
+
+    error_val(i,1) = linearRegCostFunction(Xval, yval, theta, 0);
+    
+end
 
 
 
